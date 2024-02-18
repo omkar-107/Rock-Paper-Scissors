eae8e3429edbba012a85af65330f47eef598afec
omkar <salunkheomkar107@gmail.com>
2024-02-18 14:51:39 +0530

    Initial Commit

---
+++ SPS/SPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPSApp.swift
+//  SPS
+//
+//  Created by mini project on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
